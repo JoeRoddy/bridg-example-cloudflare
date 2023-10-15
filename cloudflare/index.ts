b8245f8931a0047f2b19929c7009d503ea136e0e
Joe Roddy <mrjoeroddy@gmail.com>
2023-10-15 11:56:30 -0400

    simplify cloudflare utilities

--- cloudflare/index.ts
+++ cloudflare/index.ts
@@ -1,6 +1,7 @@
 import { FetchEvent } from '@cloudflare/workers-types';
 import { PrismaClient } from '@prisma/client/edge';
 import { handleRequest } from 'bridg/server/request-handler';
+
 const db = new PrismaClient();
 
 // @ts-ignore
@@ -10,12 +11,7 @@ async function handleEvent(event: FetchEvent): Promise<Response> {
   const { request } = event;
   const body = await getRequestBody(request);
 
-  const userId = 'cld4ar9fg000clfd96gujbblu';
-  const { data, status } = await handleRequest(body, {
-    db,
-    uid: undefined,
-    rules: { default: true },
-  });
+  const { data } = await handleRequest(body, { db, uid: 'MOCK_UID', rules: { default: true } });
 
   return getJsonResponse(data);
 }
@@ -24,7 +20,6 @@ const getJsonResponse = (data: {}) =>
   new Response(JSON.stringify(data), {
     headers: {
       'content-type': 'application/json;charset=UTF-8',
-
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Credentials': 'true',
@@ -32,35 +27,14 @@ const getJsonResponse = (data: {}) =>
     },
   });
 
-async function parseBodyFromReq(request: FetchEvent['request']) {
-  const contentType = request.headers.get('content-type') || '';
-  if (contentType.includes('application/json')) {
-    return JSON.stringify(await request.json());
-  } else if (contentType.includes('application/text')) {
-    return request.text();
-  } else if (contentType.includes('text/html')) {
-    return request.text();
-  } else if (contentType.includes('form')) {
-    const formData = await request.formData();
-    const body: Record<string, any> = {};
-    for (const entry of formData.entries()) {
-      body[entry[0]] = entry[1];
-    }
-    return JSON.stringify(body);
-  } else {
-    // Perhaps some other type of data was submitted in the form
-    // like an image, or some other binary data.
-    return 'a file';
-  }
-}
+const getRequestBody = async (request: FetchEvent['request']) => {
+  const data: string = await ((request.headers.get('content-type') || '').includes('application/json')
+    ? request.json()
+    : request.text());
 
-const parseJsonSafely = (json: string) => {
   try {
-    return JSON.parse(json);
+    return JSON.parse(data);
   } catch (error) {
-    return json;
+    return data;
   }
 };
-
-const getRequestBody = (request: FetchEvent['request']) =>
-  parseBodyFromReq(request).then((json) => parseJsonSafely(json));
